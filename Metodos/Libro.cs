207847bb83d6fe30ab5a549e2ffc89ecb0bd2e6f
Bidkar Aragón Cárdenas <bidkar.aragon@uadeo.mx>
2024-03-11 08:27:38 -0700

    Método constructor

---
+++ Metodos/Libro.cs
@@ -0,0 +1,22 @@
+﻿namespace Metodos;
+
+public class Libro
+{
+    public int Id { get; private set; }
+    public string Titulo { get; set; }
+    public int Edicion { get; set; }
+    public int Año { get; set; }
+    public string Isbn { get; set; }
+    public string Portada { get; set; }
+    public string Idioma { get; set; }
+    public int NúmeroDePáginas { get; set; }
+    public string Editorial { get; set; }
+    public string Autores { get; set; }
+    public string Categoría { get; set; }
+
+    // constructor
+    public Libro()
+    {
+        Id = new Random().Next(1,1000);
+    }
+}
---
+++ Metodos/Program.cs
@@ -0,0 +1,8 @@
+﻿using Metodos;
+
+// definir un objeto de tipo Libro
+var libro1 = new Libro();
+Console.WriteLine("Id de {0}: {1}", nameof(libro1), libro1.Id);
+
+// imprimir el título
+Console.WriteLine("Titulo: {0}", libro1.Titulo);
